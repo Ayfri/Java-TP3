3960acb53623a7d54f88eab361ff67ca12c59929
Ayfri <pierre.ayfri@gmail.com>
2022-11-05 07:40:21 +0100

    Initial commit.

---
+++ src/main/java/fr/ayfri/Main.java
@@ -0,0 +1,9 @@
+package fr.ayfri;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Main {
+	public static void main(String @NotNull [] args) {
+		System.out.println("Hello world!");
+	}
+}
